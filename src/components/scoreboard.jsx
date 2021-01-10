a3885866fe2837216635ada52d4ce6c7111a76ec
Ben Szuchmacher <59138522+bszuchmacher@users.noreply.github.com>
2021-01-10 12:38:47 +0200

    tic tac toe finally uploaded

---
+++ src/components/scoreboard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// Import Storage object
+import { Storage } from './../storage/storage'
+export class Scoreboard extends React.Component {
+  state = {
+    scoreboard: []
+  }
+
+  async componentDidMount() {
+    let storage = await new Storage().getData()
+
+    this.setState({
+      scoreboard: storage
+    })
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <h1>Recent games:</h1>
+        <ul>
+          {this.state.scoreboard.map((leader, key) => {
+            return <li key={key}>{leader}</li>
+          })}
+        </ul>
+
+                {/* Link to start new game */}
+        <Link to="/board">
+          <button className="btn">Start new game</button>
+        </Link>
+      </div>
+    )
+  }
+}
