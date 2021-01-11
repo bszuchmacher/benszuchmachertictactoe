0c609d160a0a56a5de7bde243970c62f22d7426d
Ben Szuchmacher <59138522+bszuchmacher@users.noreply.github.com>
2021-01-11 15:05:59 +0200

    styling

--- src/components/board-box.jsx
+++ src/components/board-box.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Box = (props) => {
   return (
-    <button className="board_box" onClick={props.onClick}>
+    <button style={{height:"140px", width: "140px", fontSize:"48px"}} className="board_box" onClick={props.onClick}>
       {props.value}
     </button>
   );
